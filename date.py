95c4aef014f146e64615c140a2709a4755b5069d
ben08024 <ben08024@byui.edu>
2020-02-03 09:38:22 -0500

    dhzg

---
+++ date.py
@@ -0,0 +1,14 @@
+# date class to be used by assignment class
+class Date:
+    def __init__(self):
+        self.day = 1
+        self.month = 1
+        self.year = 2000
+
+    def prompt(self):
+        self.day = int(input("Day: "))
+        self.month = int(input("Month: "))
+        self.year  = int(input("Year: "))
+
+    def display(self):
+        print("{}/{}/{}".format(self.month, self.day, self.year))
